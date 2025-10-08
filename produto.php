b3699884f2746ecce9ba849869dc3625d8f296b6
Kauacomk <kauasantana2707@gmail.com>
2025-10-08 08:48:27 -0300

    Update produto.php

--- produto.php
+++ produto.php
@@ -1,3 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style1.css">
+    <title>Document</title>
+</head>
+<body>
+     <div class="resultado-container">
 <?php 
 
 $produto = $_POST ['nome_produto'];
@@ -22,9 +32,11 @@ $valor_desconto = $valor*$desconto;
 $valor_final = $valor - $valor_desconto;
 
 
-echo "o produto $produto com o valor $valor teve um desconte de $desconto e saiu por $$valor_final";
-
-
+echo "o produto $produto com o valor $valor teve um desconte de ".$desconto*100 ."% e saiu por $".number_format($valor_final,3,".",",");
+echo "<br><br>";
+echo "<a href='index.html'>volta</a>";
 ?>
-<br>
-<a href="index.html">volta</a>
+
+</div>
+</body>
+</html>
